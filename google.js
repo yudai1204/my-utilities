ae107007af1c39f1e438539537d785b87e5f0cef
yudai hiyama <yudai1204h@gmail.com>
2024-04-21 18:20:32 +0900

    youtube shorts

--- google.js
+++ google.js
@@ -1,79 +1,65 @@
-
-function googleClassAuto(){
-    if(location.href.includes("https://classroom.google.com/u/0/") || location.href === "https://classroom.google.com"){
-        chrome.storage.local.get({data:{googleClassUser: 0}},function(items){
-            const user = Number(items.data.googleClassUser); //ここの値を、実際に転移したい先のURLの数字に設定してください　 https://classroom.google.com/u/ここの数字/h
-            if(user != 0){
-                window.location = `https://classroom.google.com/u/${user}/h`;
-                setInterval(function(){
-                    const btn = document.querySelector(".uArJ5e UQuaGc.Y5sE8d");
-                    if(btn){
-                        window.location = `https://classroom.google.com/u/${user}/h`;
-                    }
-                },100);
+function googleClassAuto() {
+  if (
+    location.href.includes("https://classroom.google.com/u/0/") ||
+    location.href === "https://classroom.google.com"
+  ) {
+    chrome.storage.local.get(
+      { data: { googleClassUser: 0 } },
+      function (items) {
+        const user = Number(items.data.googleClassUser); //ここの値を、実際に転移したい先のURLの数字に設定してください　 https://classroom.google.com/u/ここの数字/h
+        if (user != 0) {
+          window.location = `https://classroom.google.com/u/${user}/h`;
+          setInterval(function () {
+            const btn = document.querySelector(".uArJ5e UQuaGc.Y5sE8d");
+            if (btn) {
+              window.location = `https://classroom.google.com/u/${user}/h`;
             }
-        })
-    }
+          }, 100);
+        }
+      }
+    );
+  }
 }
 
+youtubeShorts();
+function youtubeShorts() {
+  if (location.href.startsWith("https://www.youtube.com/shorts/")) {
+    const id = location.href
+      .split("https://www.youtube.com/shorts/")[1]
+      .split("?")[0];
 
+    console.log({ id });
 
-function googlePassAuto(){
-if(location.href.includes("https://accounts.google.com/")){
-    if(location.href.includes("&authuser=")){
-        chrome.storage.local.get({
-            data:{
-                defaultPassword: ""
-            },
-            googlePasswords:[]
-        },function(items){
-        const authorNum = location.href.slice(location.href.indexOf("&authuser=")+10,location.href.indexOf("&authuser=")+11);
-        let clicked = false;
-        let password=items.data.defaultPassword;
-        for(const pass of items.googlePasswords){
-            if(Number(authorNum) == Number(pass.num)){
-                password = pass.pass;
-            }
-        }
-        console.log(authorNum);
-        //ログインボタン押す
-        if(location.href.includes("ServiceLogin")){
-            //https://accounts.google.com/v3/signin/confirmidentifier?dsh=S1862764520%3A1674143054203375&authuser=1&continue=https%3A%2F%2Fclassroom.google.com%2Fu%2F1%2Fh&followup=https%3A%2F%2Fclassroom.google.com%2Fu%2F1%2Fh&passive=1209600&service=classroom&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AWnogHd1CKHLZ5_baeTFTi-f5WKoctg5gwPQlGN2smM1DZuCM_pBKg8Ky500Nm8B5Wa_byxHoWPh
-            console.log("ServiceLogin");
-            function clickNextBtn(){
-                console.log("loading");
-                const nextBtn = document.querySelector("button");
-                if(nextBtn){
-                    nextBtn.click();
-                    console.log("clicked");
-                }
-            }
-            setTimeout(clickNextBtn,200);
-            setInterval(inputPassword,300);
-        }else if(location.href.includes("v2/challenge")){
-            setInterval(inputPassword,300);
-        }
-        //パスワード入力する
-        function inputPassword(){
-            if(clicked){
-                return;
-            }else{
-            const passwordBox = document.getElementById("password").querySelector("input");
-                if(passwordBox){
-                    console.log("passwordBox found!")
-                    passwordBox.value = password;
-                    const nextPassBtn = document.querySelector("button.nCP5yc");
-                    if(nextPassBtn){
-                        nextPassBtn.click();
-                        clicked = true;
-                    }
-                }
-                else{
-                    console.log("passwordBox NotFound");
-                }
-            }
-        }
-    })
+    this.document.body.insertAdjacentHTML(
+      "afterbegin",
+      `<style>
+    #udai_chrome_extension_shorts_to_normal{
+      position: absolute;
+      bottom: 10px;
+      left: 10px;
+      width: 140px;
+      height: 45px;
+      line-height: 45px;
+      background-color: #3182ce;
+      color: #fff;
+      border-radius: 5px;
+      text-align: center;
+      cursor: pointer;
+      font-weight: bold;
+      z-index: 9999999;
+      font-size: 16px;
+      user-select: none;
     }
-}
+    #udai_chrome_extension_shorts_to_normal:hover {
+      background-color: #2b6cb0;
+    }
+    </style>
+    <a href="https://www.youtube.com/watch?v=${id}">
+    <div id="udai_chrome_extension_shorts_to_normal">
+      通常動画へ
+    </div>
+    </a>
+    `
+    );
+  }
 }
--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-window.addEventListener('load', (event) => {
-    chrome.storage.local.get({ 
-        doPad: false,
-        doTwitter: true,
-        doKadaiowatter: true,
-        doInputGoogle: true,
-        doGoogleClass: true
-    },function(items){
-        if(items.doPad){
-            formAutoWindow();
-        }
-        if(items.doTwitter){
-            twitterAutoAmgScript();
-        }
-        if(items.doInputGoogle){
-            googlePassAuto();
-        }
-        if(items.doGoogleClass){
-            googleClassAuto();
-        }
-    })
+window.addEventListener("load", (event) => {
+  chrome.storage.local.get(
+    {
+      doPad: false,
+      doTwitter: true,
+      doKadaiowatter: true,
+      doInputGoogle: true,
+      doGoogleClass: true,
+    },
+    function (items) {
+      if (items.doPad) {
+        formAutoWindow();
+      }
+      if (items.doTwitter) {
+        twitterAutoAmgScript();
+      }
+      if (items.doInputGoogle) {
+        googlePassAuto();
+      }
+      if (items.doGoogleClass) {
+        googleClassAuto();
+      }
+      youtubeShorts();
+    }
+  );
 });
